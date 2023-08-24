ed79729c89afcd7c866e9ae24422f3e68fa5ae52
PaulaBoshra <64934133+PaulaBoshra@users.noreply.github.com>
2023-08-24 14:01:06 +0300

    PaulaBoshra

--- task.py
+++ task.py
@@ -1,7 +1,10 @@
 def add(): #Member 1
     pass
 def subtract(): #Member 2
-    pass
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result = num1 - num2
+    print(num1 + " - " + num2 + " = " + result)
 def multiply(): #Member 3
     pass
 def divide(): #Member 4
