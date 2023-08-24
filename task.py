a918e178a5b2c662f95f858f9c6971b8a47804b8
yomna1267 <yomna1267@gmail.com>
2023-08-24 13:43:51 +0300

    divide

--- task.py
+++ task.py
@@ -1,11 +1,18 @@
-def add(): #Member 1
-    pass
-def subtract(): #Member 2
-    pass
-def multiply(): #Member 3
-    pass
-def divide(): #Member 4
-    pass
-def mod(): #Member 5
+def add():  # Member 1
     pass
 
+
+def subtract():  # Member 2
+    pass
+
+
+def multiply():  # Member 3
+    pass
+
+
+def divide():  # Member 4
+    print(6/2)
+
+
+def mod():  # Member 5
+    pass
