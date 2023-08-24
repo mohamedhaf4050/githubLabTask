28254e945e0dc4bcfcd55c489f9928211a364143
AhmedMohamed264 <medo.mohamed201958@gmail.com>
2023-08-24 14:03:38 +0300

    Implemented Add function

--- task.py
+++ task.py
@@ -1,4 +1,7 @@
 def add(): #Member 1
+    a = int(input("Enter a number: "))
+    b = int(input("Enter second number: "))
+    print("The result: ", a + b)
     pass
 def subtract(): #Member 2
     pass
