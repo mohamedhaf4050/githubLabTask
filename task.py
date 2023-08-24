84aac8ef434bdaaaf4f2feb31e66567f6fd86cda
Mohamed Ahmed <103213877+m7meda7medd@users.noreply.github.com>
2023-08-24 14:23:02 +0300

    Multiplication added

--- task.py
+++ task.py
@@ -2,8 +2,8 @@ def add(a,b): #Member 1
     return a+b
 def subtract(): #Member 2
     pass  
-def multiply(): #Member 3
-    pass
+def multiply(a,b): #Member 3
+    return a*b
 def divide(): #Member 4
     pass
 def mod(a , b): #Member 5
