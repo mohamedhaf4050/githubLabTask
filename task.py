f6965d97d4c4dab326aee98daeec77b18bd6a225
Mahmoud El-Sanour <mahmoudsanour@gmail.com>
2023-08-24 07:08:31 -0400

    Edit: task, Created divide

--- task.py
+++ task.py
@@ -4,8 +4,10 @@ def subtract(x,y): #Member 2
     return x-y
 def multiply(): #Member 3
     pass
-def divide(): #Member 4
-    pass
+def divide(x, y): #Member 4
+    if y != 0:
+        return (x / y)
+    return ("Division on zero is not allowed")
 def mod(x,y): #Member 5
     return x%y
 
