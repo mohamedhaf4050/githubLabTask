b976c5fdd847d20a6127a6880bf98e56c9ef2410
NadineMohammed <nadine.elframawy@ejust.edu.eg>
2023-08-24 14:05:48 +0300

    Nadine Subtract

--- task.py
+++ task.py
@@ -2,8 +2,15 @@ def add(x,y):  #Julia Nader
     total = x+y
     return total
 
-def subtract():  # Member 2
+def subtract(a,b): #Member 2
     pass
+    return a - b
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = subtract(num1, num2)
+print("The result of subtracting", num2, "from", num1, "is:", result)
 
 
 def multiply():  # Member 3
