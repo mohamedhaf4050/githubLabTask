52d249cbd3a02c61720abec1727352253869a474
MousaAshraf <123460138+Boshkkash@users.noreply.github.com>
2023-08-24 14:03:47 +0300

    Update task.py
    Div Function

--- task.py
+++ task.py
@@ -4,8 +4,16 @@ def subtract(): #Member 2
     pass
 def multiply(): #Member 3
     pass
-def divide(): #Member 4
-    pass
+def divide_numbers():
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if num2 != 0:
+        result = num1 / num2
+        print("The result of division is:", result)
+    else:
+        print("Error: Cannot divide by zero.")
+divide_numbers()
 def mod(): #Member 5
     pass
 
