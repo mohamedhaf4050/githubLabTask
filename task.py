18536d9039967cc814592abb302848dfc362c405
Mahmoud El Gohary <mahmodudgo1000@gmail.com>
2023-08-24 14:05:16 +0300

    addint the code to multiply two numbers

--- task.py
+++ task.py
@@ -2,8 +2,8 @@ def add(x,y): #Member 1
     return x+y
 def subtract(): #Member 2
     pass
-def multiply(): #Member 3
-    pass
+def multiply(x, y): #Member 3 - Mahmoud El Goahry
+    return x * y
 def divide(): #Member 4
     pass
 def mod(x,y): #Member 5
