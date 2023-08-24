e2cca8eed807ac7bd7df3126d83134cdb4a95831
PaulaBoshra <64934133+PaulaBoshra@users.noreply.github.com>
2023-08-24 14:34:48 +0300

    Final Task

--- task.py
+++ task.py
@@ -1,20 +1,20 @@
-def add(): #Member 1
+def add(): #Member Ahmed
     a = int(input("Enter a number: "))
     b = int(input("Enter second number: "))
     print("The result: ", a + b)
-    pass
-def subtract(): #Member 2
+
+def subtract(): #Member Paula
     num1 = int(input("Enter the first number: "))
     num2 = int(input("Enter the second number: "))
     result = num1 - num2
     print(num1 + " - " + num2 + " = " + result)
-def multiply(): #Member 3Saher
+
+def multiply(): #Member Saher
     x=int(input())
     y=int(input())
     print(x*y)
-    
 
-def divide(): #Member 4
+def divide(): #Member Elrawi
     number_1 = float(input("Enter First Number: "))
     number_2 = float(input("Enter Second Number: "))
     result = number_1 / number_2
