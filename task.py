3fb9751d27279026e493745a3b4dacae573e71d0
ammarkhattab <75506841+ammarkhattab@users.noreply.github.com>
2023-08-24 14:07:15 +0300

    Added Division Function

--- task.py
+++ task.py
@@ -9,3 +9,14 @@ def divide(): #Member 4
 def mod(): #Member 5
     pass
 
+def divide(): 
+    try:
+        num1 = float(input("Enter the first number: "))
+        num2 = float(input("Enter the second number: "))
+        result = num1 / num2
+        print("Result of division:", result)
+        
+    except ValueError:
+        print("Error: Invalid input. Please enter valid numbers.")
+
+
