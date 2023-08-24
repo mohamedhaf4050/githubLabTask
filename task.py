fad2335420cb50db9262387892f112142a5765ce
Amr789 <79154715+Amr789@users.noreply.github.com>
2023-08-24 14:26:18 +0300

    multiplication

--- task.py
+++ task.py
@@ -2,7 +2,9 @@ def add(): #Member 1
     pass
 def subtract(): #Member 2
     pass
-def multiply(): #Member 3
+def multiply(num1,num2): #Member 3
+    result=num1*num2
+    return result
     pass
 def divide(): #Member 4
     pass
